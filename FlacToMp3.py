d29b1ab05abaf3375b950d4840aa2cd00540aca0
Bartek <bartek@Laptop-Linux>
2015-04-04 14:12:04 +0200

    Initial commit.

---
+++ FlacToMp3.py
@@ -0,0 +1,12 @@
+#!/bin/python
+__author__ = 'Kordek Bartłomiej'
+
+def CheckForNecessaryApplications:
+
+
+def main():
+    print "Flac to mp3 converter."
+    sasa
+
+if __name__ == "__main__":
+    main()
